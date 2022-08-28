76f5c05cf9b5eb5cee3318e4f2b2b76fac972cbf
andrea-gi <andrea-gi@users.noreply.github.com>
2022-08-28 00:10:13 -0500

    Added discount by month

--- SubscribeME/src/utils/statsCalculator.js
+++ SubscribeME/src/utils/statsCalculator.js
@@ -5,13 +5,29 @@ function computeStatsCategories(subs) {
   for (const s of subs) {
     if (s.category in subPerCategory) {
       subPerCategory[s.category]++;
-      costPerCategory[s.category] += s.price;
+      costPerCategory[s.category] += computeMonthDiscount(
+        s.price,
+        s.renewalPeriod
+      );
     } else {
       subPerCategory[s.category] = 1;
-      costPerCategory[s.category] = s.price;
+      costPerCategory[s.category] = computeMonthDiscount(
+        s.price,
+        s.renewalPeriod
+      );
     }
   }
   return { subPerCategory: subPerCategory, costPerCategory: costPerCategory };
+}
+
+function computeMonthDiscount(cost, period) {
+  if (period === "month" || period === "none") {
+    return Math.round(cost);
+  } else if (period === "week") {
+    return Math.round(cost * 4.35);
+  } else if (period === "year") {
+    return Math.round(cost / 12);
+  }
 }
 
 export { computeStatsCategories };
